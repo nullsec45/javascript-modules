058cb2e147c558ad922b20989854ce332c80ee86
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-14 17:11:51 +0700

    axport import alias

---
+++ scripts/alias.js
@@ -0,0 +1,11 @@
+const name="Rama Fajar Fadhillah";
+
+function sum(first, second){
+    return first+second;
+}
+
+class Company{
+
+}
+
+export {name as nama, sum as total, Company as Perusahaan};
